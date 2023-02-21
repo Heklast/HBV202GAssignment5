b39363d90228a28f40882ec18e2ae3d4c6531f2f
Heklast <hst10@hi.is>
2023-02-21 11:54:36 +0000

    commit-a allt

--- src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
+++ src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
@@ -1,17 +1,70 @@
 package is.hi.hbv202g.ass5;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class IntStackTest {
+    IntStack stack;
+
+    @Before
+    public void constructStackTestObject() {
+        stack = new IntStack();
+    }
 
     @Test
     public void testNewStackIsNotFull() {
-        IntStack stack= new IntStack();
+        //IntStack stack = new IntStack();
 
         assertFalse(stack.isFull());
     }
 
+    @Test
+    public void testFullStackIsFull() {
+        int N = stack.getCapacity();
+
+        for (int i = 1; i <= N; i++) {
+            stack.push(i);
+        }
+        assertTrue(stack.isFull());
+    }
+
+    @Test
+    public void testAlmostFullStack() {
+        int N = stack.getCapacity();
+
+        for (int i = 1; i <= N; i++) {
+            stack.push(i);
+        }
+        stack.pop();
+        assertFalse(stack.isFull());
+    }
+
+    @Test
+    public void testPopReturnsPushedValue() {
+        stack.push(10);
+        assertEquals(10, stack.pop());
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testPopOnEmptyStack() {
+        stack.pop();
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testPushOnFullStack() {
+        int N = stack.getCapacity();
+        for (int i = 1; i <= N; i++) {
+            stack.push(i);
+        }
+        stack.push(11);
+    }
+
+    @Test
+    public void testAlmostEmptyStack() {
+        stack.push(1);
+        assertFalse(stack.isEmpty());
+    }
 }
+
